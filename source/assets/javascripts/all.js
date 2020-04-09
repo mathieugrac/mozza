107f297b1f48e817c2a7d2881beeb0d610c25b79
Mathieu Grac <mathieugrac@gmail.com>
2020-04-09 14:30:04 +0100

    uxr updates

--- source/assets/javascripts/all.js
+++ source/assets/javascripts/all.js
@@ -1,1 +1,19 @@
+//= require jquery
 //= require_tree .
+
+
+$(document).ready(function() {
+
+	var heroHeight = $(".uxr-hero").outerHeight();
+	console.log(heroHeight);
+
+	$(window).scroll(function() {
+		var height = $(window).scrollTop();
+		if(height > heroHeight) {
+			$('.moon').addClass('active');
+		} else {
+			$('.moon').removeClass('active');
+		}
+	});
+
+});
